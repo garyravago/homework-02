fd098c395fb98c43bdb887e06545f7db782ae358
Gary Ravago <ravagog@gmail.com>
2017-08-07 23:09:12 -0700

    Add additional book search attributes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   def self.search(search)
-    where("lower(title) LIKE ?", "%#{search.downcase}%")
+    where("lower(title) LIKE ? OR lower(author) LIKE ? OR lower(classification) LIKE ? OR lower(genre) LIKE ? OR lower(category) LIKE ?",
+     "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%")
   end
 end
