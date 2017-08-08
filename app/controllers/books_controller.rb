8acf39ed05de52a54cf38948d19f6dc3bf60c78f
Gary Ravago <ravagog@gmail.com>
2017-08-07 21:36:25 -0700

    Add edit book action and form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,19 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.update(book_params)
+    redirect_to book_url
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :genre, :classification, :category, :year)
+  end
 end
