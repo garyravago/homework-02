97d5bc9555b73e1dc9114e4e7f33130c7e229b5c
Gary Ravago <ravagog@gmail.com>
2017-08-14 20:02:22 -0700

    Add Author model and remove author from Books

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170808030015) do
+ActiveRecord::Schema.define(version: 20170815025904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "author"
     t.string "genre"
     t.string "classification"
     t.string "category"
