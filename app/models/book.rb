4de49d20cd2443170c99437e39d34bae72f434a2
Gary Ravago <ravagog@gmail.com>
2017-08-14 23:07:19 -0700

    Fix search query and add new author fields

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
   has_many :authors, through: :authorships
 
   def self.search(search)
-    where("lower(title) LIKE ? OR lower(classification) LIKE ? OR lower(genre) LIKE ? OR lower(category) LIKE ?",
-     "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%")
+    joins(:authors).where("lower(first_name) LIKE ? OR lower(last_name) LIKE ? OR lower(title) LIKE ? OR lower(classification) LIKE ? OR lower(genre) LIKE ? OR lower(category) LIKE ?",
+     "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%")
   end
 end
