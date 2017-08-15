1e74405417b7d1fbc689034482748573840c8e9a
Gary Ravago <ravagog@gmail.com>
2017-08-14 20:15:21 -0700

    Add Author new and show action

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,17 +1,30 @@
 class AuthorsController < ApplicationController
 
+	def show
+		@author = Author.find(params[:id])
+	end
+
 	def new
+		@author = Author.new
 	end
 
 	def edit
 	end
 	
 	def create
+		@author = Author.create(author_params)
+		redirect_to author_url(@author)
 	end
 
 	def update
 	end
 
 	def destroy
+	end
+
+	private
+		
+	def author_params
+		params.require(:author).permit(:first_name, :last_name, :age)
 	end
 end
